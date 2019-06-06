382e97ab7c43748a7e68ea65bdcaa4ed659a5d28
edinha <edinha.gon.cruz@gmail.com>
2019-06-06 19:26:01 -0300

    Start tkinter

---
+++ 03/src/demo/tinder.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+window = Tk()
+window.title("Welcome to LikeGeeks app")
+window.geometry('480x720')
+
+canvas = Canvas(window, width=480, height=720)
+canvas.pack()
+
+
+
+window.mainloop()
