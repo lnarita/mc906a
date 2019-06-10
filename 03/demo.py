e9ce18ed8498152ba25e16b750b0dfe7f1535d27
Lucy Narita <13240125+lnarita@users.noreply.github.com>
2019-06-09 22:18:33 -0300

    swipe up for best gril

--- 03/demo.py
+++ 03/demo.py
@@ -1,9 +1,10 @@
 import sys
 import time
 import random
-sys.path.insert(0, '/Users/williamcruz/warzone/mc906a/03/src')
 
 import matplotlib
+import numpy as np
+
 matplotlib.use("TKAgg")
 
 from tkinter import *
@@ -22,8 +23,21 @@ class DemoTinderWindow():
 
 	def should_recomend_waifu(self, waifu):
 		######## TODO Right way of actually recommend waifus ##########
-		print("RECOMENDER PROBABILITY CALCULATOR: ", Recommender.calculate_like_probability(self.profile, waifu))
-		return Recommender.calculate_like_probability(self.profile, waifu) > 4.0
+		likeness = Recommender.calculate_like_probability(self.profile, waifu)
+		print("RECOMENDER PROBABILITY CALCULATOR: ", likeness)
+		trash = fuzz.interp_membership(np.arange(0, 11, 1), self.profile.likeness['low'].mf, likeness)
+		waifu = fuzz.interp_membership(np.arange(0, 11, 1), self.profile.likeness['average'].mf, likeness)
+		best_gril = fuzz.interp_membership(np.arange(0, 11, 1), self.profile.likeness['high'].mf, likeness)
+		if best_gril > waifu and best_gril > trash:
+			return 'B'
+		if waifu > best_gril and waifu > trash:
+			return 'W'
+		if trash > waifu and trash > best_gril:
+			return 'T'
+		if likeness > 4.0:
+			return 'W'
+		else:
+			return 'T'
 
 	def get_waifu_card(self, waifu):
 		card = self.canvas.create_rectangle(0, 0, 480, 720, fill="#CACACA")
@@ -38,20 +52,40 @@ class DemoTinderWindow():
 		self.canvas = Canvas(window, width=WIDTH, height=HEIGHT)
 		self.canvas.pack()
 
+		def _swipe_up():
+			for y in range(0, int(HEIGHT/10)):
+				self.canvas.move(card, 0, -10)
+				self.canvas.move(label, 0, -10)
+				window.update()
+				time.sleep(.01)
+
+		def _swipe_right():
+			for x in range(0, int(WIDTH/10)):
+				self.canvas.move(card, 10, 0)
+				self.canvas.move(label, 10, 0)
+				window.update()
+				time.sleep(.01)
+
+		def _swipe_left():
+			for x in range(0, int(WIDTH/10)):
+				self.canvas.move(card, -10, 0)
+				self.canvas.move(label, -10, 0)
+				window.update()
+				time.sleep(.01)
+
 		cards = []
 		for waifu in WAIFUS:
 			(card, label) = self.get_waifu_card(waifu)
-			is_recommended = self.should_recomend_waifu(waifu)
+			category = self.should_recomend_waifu(waifu)
+			swipe = {
+				'B': lambda: _swipe_up(),
+				'W': lambda: _swipe_right(),
+				'T': lambda: _swipe_left()
+			}[self.should_recomend_waifu(waifu)]
 			window.update()
 			time.sleep(2)
 
-			new_position = 10 if is_recommended else -10
-
-			for x in range(0, int(WIDTH/10)):
-				self.canvas.move(card, new_position, 0)
-				self.canvas.move(label, new_position, 0)
-				window.update()
-				time.sleep(.01)
+			swipe()
 
 		self.canvas.tag_lower(card)
 		self.canvas.tag_lower(label)
@@ -59,6 +93,5 @@ class DemoTinderWindow():
 		window.mainloop()
 
 
-
-# Running...
-DemoTinderWindow(ShortStacksLover()).main_loop()
+if __name__ == '__main__':
+	DemoTinderWindow(ShortStacksLover()).main_loop()
